207a993a49bb281c5446a00ad36c7085fa436e26
philippspo <philipp.sporrer@commercetools.de>
2016-06-13 22:23:50 +0200

    setup testing with mocha, chai and sinon

---
+++ src/TodoApp/server/publications.tests.js
@@ -0,0 +1,9 @@
+// import { Meteor } from 'meteor/meteor'
+import { expect } from 'chai'
+import sinon from 'sinon'
+
+describe('publications', function () {
+  it('should do something', function () {
+    expect(true).to.equa(false)
+  })
+})
